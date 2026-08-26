ff118dd2265b4e939d79421c794623ac32037953
samruddhi0113 <samruddhi13jan@gmail.com>
2026-08-26 21:06:55 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,14 @@
+<?php
+
+   
+    $num1=20;
+    $num2=40;
+    $ans=$num1+$num2;
+   echo"Addition=".$ans;<br>
+   $sub=num2-num1;
+   echo"Subtraction=".$sub;<br>
+  $multi=num1*num2;
+ echo"Multiplication =".$multi;<br>
+ $division=$num1/$num2;
+echo"Division=".$division;
+?>
